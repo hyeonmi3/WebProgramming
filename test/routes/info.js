3d8b361f089505d4ccdf664d53bed434d2e25b42
hyeonmi3 <cc146@daum.net>
2022-10-04 15:09:27 +0900

    web 수업

--- test/routes/info.js
+++ test/routes/info.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('info', { title: 'Express' }, { message: '아무거남'});
+router.get('/', (req, res) => {
+  res.render('info');
 });
+
+router.post('/update', (req, res) => {
+  const title = req.body.title;
+  res.send(`Title is ${title}`);
+})
 
 module.exports = router;
--- test/routes/users.js
+++ test/routes/users.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
+  // 단순 배열
+  const message = ['Hello', 'Everyone', '!'];
+
+  // 객체의 배열
   const users = [
-    { id: 1, name: 'user1'},
-    { id: 2, name: 'usre2'}
-  ];
-  res.json(users);
+    {userId: 'userA', name: 'AAA'},
+    {userId: 'userB', name: 'BBB'},
+    {userId: 'userC', name: 'CCC'},
+  ]
 });
+
+router.get('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  res.send(`Hello ${userId}`);
+})
 
 module.exports = router;
