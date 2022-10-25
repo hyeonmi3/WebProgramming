ff3c7081b34de866f25178561b91f5c86c0b54fc
hyeonmi3 <cc146@daum.net>
2022-10-25 15:39:41 +0900

    web 수업

--- test/routes/index.js
+++ test/routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
-const tododb = require('../models/mariadb/todo');
+const Todo = require('../models/sequelize/todoModel');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', async(req, res) => {
-  const rows = await tododb.selectTodos();
+  // const rows = await tododb.selectTodos();
+  try{
+  const rows = await Todo.findAll();
   res.render('index', { todos:rows });
+  }catch(err){
+    console.log(err);
+    res.render('index');
+  }
 });
 
 module.exports = router;
